1bc80e42c0e7e93161962ea667505709b4b0ea82
MArslan88 <gery_maz_555@hotmail.com>
2022-06-26 16:55:02 +0500

    assets updated

--- lib/constants/asset_constants.dart
+++ lib/constants/asset_constants.dart
@@ -4,4 +4,8 @@ class AssetConstants {
   static String homeBackground = 'assets/images/home_background.png';
   static String fwdArrowIcon = 'assets/images/fwd_arrow.png';
   static String dropDownIcon = 'assets/images/drop_down.png';
+  static String backIcon = 'assets/images/back_arrow.png';
+  static String currentLocationIcon = 'assets/images/current_location_icon.png';
+  static String refreshIcon = 'assets/images/refresh_icon.png';
+  static String cloudIcon = 'assets/images/cloud_icon.png';
 }
