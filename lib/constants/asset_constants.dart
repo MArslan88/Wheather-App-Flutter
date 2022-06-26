aff72a3a258c16f9b292a166bd021cd23e6bf69f
MArslan88 <gery_maz_555@hotmail.com>
2022-06-26 14:54:04 +0500

    new assets included

--- lib/constants/asset_constants.dart
+++ lib/constants/asset_constants.dart
@@ -1,3 +1,5 @@
 class AssetConstants {
   static String splashScreenBackground = 'assets/images/splash_icon.png';
+  static String landingPageBackground = 'assets/images/flow_line.png';
+  static String homeBackground = 'assets/images/home_background.png';
 }
