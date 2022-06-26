449c178eeafb6257e872c267eae035e2276bd571
MArslan88 <gery_maz_555@hotmail.com>
2022-06-26 16:37:28 +0500

    style updated

--- lib/shared/style.dart
+++ lib/shared/style.dart
@@ -6,15 +6,11 @@ import 'package:google_fonts/google_fonts.dart';
 
 // App Colors
 const Color appGreenColor = Color(0xFF6CC51D);
-const Color appGreyColor = Color(0xFF868889);
+const Color appGreyColor = Color(0xFFE0E0E0);
 const Color appWhiteColor = Color(0xFFF4F5F9);
-const Color appBlueColor = Color(0x6DC9EF);
-const Color appGreenSecondary = Color(0xFFAEDC81);
-const Color appGreySecondary = Color(0xFFEBEBEB);
+const Color appBlueColor = Color(0xFF6DC9EF);
 const Color appGreyDark = Color(0xFFB1B1B1);
-const Color appGreyLight = Color(0xF4F5F9);
-const Color appRedColor = Color(0xFFEF574B);
-const Color appBlackColor = Color(0x000000);
+const Color appBlackColor = Color(0xFF000000);
 
 TextStyle heading1 = GoogleFonts.poppins(
   fontSize: 20,
@@ -23,8 +19,19 @@ TextStyle heading1 = GoogleFonts.poppins(
   color: appBlackColor,
 ) as TextStyle;
 
+TextStyle heading2 = GoogleFonts.poppins(
+  fontSize: 57,
+  fontWeight: FontWeight.w600,
+  color: appBlackColor,
+) as TextStyle;
+
 TextStyle heading4 = GoogleFonts.poppins(
   fontSize: 17,
   fontWeight: FontWeight.w500,
-  color: Colors.black,
+  color: appBlackColor,
+) as TextStyle;
+
+TextStyle heading6 = GoogleFonts.poppins(
+  fontSize: 20,
+  color: appBlackColor,
 ) as TextStyle;
